565a6eeb3bd0a7bd53c023255c328071c66aa2e5
Jaws8181 <jamesallenwalsh@gmail.com>
2026-07-25 14:25:15 -0400

    update

--- js/inventory.js
+++ js/inventory.js
@@ -277,7 +277,7 @@ const Inventory = {
             statusBadge = '<span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-900/30 text-green-400 border border-green-800/50">In Stock</span>';
         }
 
-        const safeId = JSON.stringify(item.id);
+        const safeId = `'${item.id}'`;
 
         // Order form toggle pill
         const toggleBtn = Auth.canViewFinancials() ? `
